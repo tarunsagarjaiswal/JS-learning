124340a1ca5950971c77dfdafc4b69a59d1837eb
tarunsagarjaiswal <tarunsagarjaiswal19@gmail.com>
2026-06-08 17:31:10 +0000

    variables and datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,12 @@
+const accountID = 129384
+let accountEmail = "tarun@gmail.com"
+var accountPassword = "12345"
+accountCity = "Ujjain"
+
+// accountID = 2334  not allowed bcauz const cant be changed
+console.log(accountID);
+accountEmail = "hell@hc.com"
+accountPassword = "456010"
+accountCity = "Indore"
+
+console.table([accountID, accountEmail, accountPassword, accountCity])
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,22 @@
+"use strict";
+
+// alert(3 + 3)   //error due to node js
+// console.log(3 + 3);  console.log("Tarun")
+
+// or 
+
+
+//its readability is high, so we prefer this
+console.log(3 + 3)
+
+console.log("Tarun")
+
+let  name = "hellNah"
+let age = 18
+let isLoggedIn = true
+let state;
+
+console.log(typeof name);
+console.log(typeof age);
+console.log(typeof state);
+console.log(typeof null)
