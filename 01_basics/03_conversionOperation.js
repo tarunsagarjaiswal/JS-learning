c5b71851102e21b2b1d31d60cbeef723328d1f6e
tarunsagarjaiswal <tarunsagarjaiswal19@gmail.com>
2026-06-09 06:45:50 +0000

    added operations also

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -13,3 +13,38 @@ let isLoggedInBool = Boolean(isLoggedIn)
 // console.log(typeof isLoggedInBool)
 
 // console.log(isLoggedInBool)
+
+// ************************ Operations *********************************
+let value = 3
+
+let negValue = -value
+// console.log(negValue)
+
+let str1 = "Hi"
+let str2 = " Tarun"
+let str3 = str1 + str2
+// console.log(str3);
+
+// console.log("1" + 2 + 2)
+
+// console.log(1 + 2 + "2")
+
+// console.log("1" + 2)
+
+// console.log( 2 + "1" )
+
+console.log(true)
+console.log(+true)
+// console.log(true+)   //error
+
+let num1, num2, num3;
+
+num1 = nums2 = nums3 = 2 + 3        //not a good way of writting
+
+let gameCounter = 100;
+gameCounter++;
+
+console.log(gameCounter)
+
+// alert("hello tarun")   //pop appear on browser and not on the node js
+
