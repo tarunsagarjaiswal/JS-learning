f7cc0c4b3254f04e2219c887640536a926c7cd05
tarunsagarjaiswal <tarunsagarjaiswal19@gmail.com>
2026-06-09 07:16:42 +0000

    comparision

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,20 @@
+//Basic comaprision
+// console.log(2 > 1)
+// console.log(2 >= 1)
+// console.log(2 < 1)
+// console.log(2 == 1)
+// console.log(2 != 1)
+
+//doubtful or confusing comaprision
+
+// console.log(null > 0)
+// console.log(null == 0)
+// console.log(null <= 0)
+
+// console.log(undefined > 0)
+// console.log(undefined == 0)
+// console.log(undefined >= 0)
+
+// ===  strict check like also checks the datatype for comaprision
+console.log("2" == 2) 
+console.log("2" === 2) 
