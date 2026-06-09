932cc8c0c406910686061997412c87b25d50db3b
tarunsagarjaiswal <tarunsagarjaiswal19@gmail.com>
2026-06-09 17:11:10 +0000

    nums and maths in js

---
+++ 01_basics/06_nums_and_maths.js
@@ -0,0 +1,37 @@
+const score = 400
+// console.log(score)
+
+const balance = new Number(100.3334)
+// console.log(balance);
+
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(2));
+
+const otherNumber = 123.7578;
+
+// console.log(otherNumber.toPrecision(1));
+
+const hundreds = 1000000000
+// console.log(hundreds.toLocaleString("en-IN"));
+
+
+//********************************************* MATHS ******************************************************
+
+// console.log(Math)
+// console.log(Math.abs(-100));
+// console.log(Math.round(47.499));
+// console.log(Math.ceil(47.000001));
+// console.log(Math.floor(47.9999999999999));
+
+// console.log(Math.min(3,4,6,-1,4,5));
+// console.log(Math.max(3,4,6,-1,4,5));
+
+console.log(Math.random())
+
+console.log(Math.floor(Math.random() * 10) + 1);    //range of number is [1, 10]
+
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random() * (max - min + 1)) + min);     //range of number is [min, max]
+
