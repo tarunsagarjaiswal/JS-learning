e1f4d762bab677fd4b018ea8aaa7e8e9352bd304
tarunsagarjaiswal <tarunsagarjaiswal19@gmail.com>
2026-06-09 06:29:28 +0000

    conversion of datatype

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,15 @@
+let score = undefined
+
+// console.log(typeof(score))
+
+let valueInNumber = Number(score)
+
+// console.log(typeof valueInNumber)
+// console.log(valueInNumber)
+
+let isLoggedIn = null;
+let isLoggedInBool = Boolean(isLoggedIn)
+
+// console.log(typeof isLoggedInBool)
+
+// console.log(isLoggedInBool)
