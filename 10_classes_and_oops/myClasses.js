f3ba78445e96b9a1604bb9601a289c3020362693
Tarun Sagar Jaiswal <tarunsagarjaiswal19@gmail.com>
2026-06-19 16:38:18 +0530

    about classes in js

---
+++ 10_classes_and_oops/myClasses.js
@@ -0,0 +1,47 @@
+//we are talking after ES6 things
+
+class User {
+    constructor(username, email, password) {     //when you intialize objects with the new keyword then constructor is called.
+        this.username = username;
+        this.email = email;
+        this.password=  password;
+    }
+
+    //in class we make fxns like this format
+    encryptPass() {
+        return `${this.password}#$@%!`
+    }
+
+    changeUserName() {
+        return this.username.toUpperCase();
+    }
+};
+
+const user = new User("tarunsaavi", "T&S@google.com", 43267);
+
+console.log(user.encryptPass());
+console.log(user.changeUserName());
+
+
+/*
+//Behind the scene of the class
+function setDetail(username, email, password) {
+    this.username = username;
+    this.email = email;
+    this.password=  password;
+}
+
+setDetail.prototype.encryptPass = function () {
+    return `${this.password}#$@%!`
+}
+
+setDetail.prototype.changeUserName = function () {
+    return this.username.toUpperCase();
+}
+
+const tea = new setDetail("tarunsaavi", "T&S@google.com", 43267)
+
+console.log(tea.encryptPass());
+
+console.log(tea.changeUserName());
+*/
